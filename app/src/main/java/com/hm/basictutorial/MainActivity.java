f2f022ef1c293eb16959e1298f63ec4dc07189ce
HM <hello@hazella.co>
2018-07-18 23:35:04 -0500

    Link sendMessage() to button

--- app/src/main/java/com/hm/basictutorial/MainActivity.java
+++ app/src/main/java/com/hm/basictutorial/MainActivity.java
@@ -1,7 +1,8 @@
 package com.hm.basictutorial;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+        Sends a message when button is pressed.
+     */
+    public void sendMessage(View view) {
+        // Do something
+        System.out.println("Hello WORLD!");
     }
 }

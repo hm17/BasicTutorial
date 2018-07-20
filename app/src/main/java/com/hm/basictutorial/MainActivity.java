7d06e15a35bc315430b1075b469a9c395cdbb7fe
HM <hello@hazella.co>
2018-07-20 13:08:43 -0500

    Finished building Basic Tutorial

--- app/src/main/java/com/hm/basictutorial/MainActivity.java
+++ app/src/main/java/com/hm/basictutorial/MainActivity.java
@@ -1,10 +1,14 @@
 package com.hm.basictutorial;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.hm.basictutorial.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +20,10 @@ public class MainActivity extends AppCompatActivity {
         Sends a message when button is pressed.
      */
     public void sendMessage(View view) {
-        // Do something
-        System.out.println("Hello WORLD!");
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
